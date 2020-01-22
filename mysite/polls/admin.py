779828355d7202619d88fc2eb6e27ab0609a0be9
S. M. JAHANGIR <smjxpro@gmail.com>
2020-01-22 15:13:10 +0600

    adding choices directly from question admin form using StackedInline

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']})
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
